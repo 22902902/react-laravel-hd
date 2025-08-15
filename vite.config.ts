a45a58a5317a09a11305ba3101e1703e7054ccea
“22902902” <7242783@qq.com>
2025-08-15 16:22:51 +0800

    安装配置tanstack router路由

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
-
+// UI库：ui.shadcn.com ，样式库：tailwindcss.com ，图标库：icones.js.org
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
